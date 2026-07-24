815457e2892c997e3034d29fec8bce627b8b6f5c
DanilloAmassaBuxa <daniloluiz3605569@gmail.com>
2026-07-24 13:08:57 -0300

    Add files via upload

---
+++ skibidijava123/javinha.js
@@ -0,0 +1,23 @@
+console.log("skibidi toilet")
+console.log("tchau")
+alert("ta travaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaan")
+confirm("deseja vender seus dados para o Trump?")
+
+let nome_bixiga = "baba atomica de dinosauros engolidores de morangos" 
+
+let senha_do_cartao = "123456789"
+console.log (senha_do_cartao)
+
+let idade = 20+20+20+7
+console.log(idade)
+ 
+let numero_de_corpos_no_porao = 1+1+1+1+1+1+1+1+1+1+1+1+1+1+1
+console.log(numero_de_corpos_no_porao + "corpos")
+
+console.log(67>69)
+let nome_bixiga = prompt("whats your name nigga")
+
+let nome = prompt("se identifique neguinho")
+if (nome=="danilao"){
+    alert("dessa vez passa")
+ } if
---
+++ skibidijava123/outra_coisa.js
@@ -0,0 +1,11 @@
+console.log ("ola macaquito")
+console.log("passou dessa vez")
+alert("bem vindo desconhecido")
+confirm("voce aceita vender seus dados para a coreia do norte?")
+
+let nome = "desconhecido por enquanto"
+console.log(nome)
+let cidade = "desconhecido_por_enquanto"
+console.log (cidade)
+let idade = "desconhecido por enquanto"
+console.log (idade)
